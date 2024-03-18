9dad70ae7f777b537333745d017f09a8741eb7d4
Wes H <wwh.3002@gmail.com>
2024-03-18 08:44:07 -0700

    Edited side-nav to have all links in JavaScript and utilize a ngForOf loop.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'search', component: SearchPageComponent },
+];
--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [MatListModule, MatIconModule],
+  imports: [
+    MatListModule,
+    MatIconModule,
+    NgForOf
+  ],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css'
 })
 export class SideNavComponent {
 
+  links: LinkModel[] = [
+    {
+      title: 'Home Page',
+      icon: 'home',
+      link: ''
+    },
+    {
+      title: 'Search Page',
+      icon: 'search',
+      link: 'search'
+    }
+  ]
+
+  changeRoute = (route: string) => {
+    window.location.href = route;
+  }
+}
+
+interface LinkModel {
+  link: string;
+  icon: string;
+  title: string;
 }
